2ddaeac3ee4a8743d1c8167437c8e2f7536e0e50
n800sau <n800sau@mail.com>
2024-01-04 17:49:33 +1100

    added scripts

--- wifi/wifi_ap/firmware/ESP32-Camera/flash.sh
+++ wifi/wifi_ap/firmware/ESP32-Camera/flash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 --before default_reset --after no_reset write_flash -z \
+esptool.py --chip esp32 --port /dev/ttyUSB1 --baud 921600 --before default_reset --after no_reset write_flash -z \
 --flash_mode dio --flash_freq 80m --flash_size detect \
 0x1000 bootloader.bin \
 0x10000 camera.bin \
